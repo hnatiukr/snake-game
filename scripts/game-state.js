dcbedaf04839d63b23f66ba1d3d1540051e7a2e7
Roman Hnatiuk <roman.hnatiuk@openvpn.net>
2022-03-15 23:54:14 +0200

    refactor: move gameplay functions from interactions file

--- scripts/game-state.js
+++ scripts/game-state.js
@@ -118,3 +118,60 @@ function updateTimeout() {
 
   return timeout;
 }
+
+function stopGame(isSuccessfully) {
+  gameOver = true;
+  gameOverSound.play();
+
+  scoreInGame.style.visibility = "hidden";
+  gameOverScreen.style.visibility = "visible";
+  canvas.style.borderColor = isSuccessfully ? "darkgreen" : "#FFD829";
+
+  saveScore();
+  clearInterval(gameInterval);
+}
+
+function startGame() {
+  gameOver = false;
+  startSound.play();
+
+  score = 0;
+  timeout = 80;
+  directionQueue = [];
+  currentDirection = moveRight;
+  currentSnake = makeInitialSnake(5);
+
+  currentSnakeKeys = new Set();
+  currentVacantKeys = new Set();
+
+  scoreInGame.style.visibility = "visible";
+  gameOverScreen.style.visibility = "hidden";
+
+  for (let top = 0; top < ROWS; top += 1) {
+    for (let left = 0; left < COLS; left += 1) {
+      currentVacantKeys.add(toKey([top, left]));
+    }
+  }
+
+  for (let cell of currentSnake) {
+    const key = toKey(cell);
+
+    currentVacantKeys.delete(key);
+    currentSnakeKeys.add(key);
+  }
+
+  currentFoodKey = spawnFood();
+
+  const [snakeKeys, vacantKeys] = partitionCells(currentSnake);
+
+  currentSnakeKeys = snakeKeys;
+  currentVacantKeys = vacantKeys;
+
+  canvas.style.borderColor = "";
+  gameOverScore.innerHTML = score;
+  scoreInGame.innerHTML = score;
+
+  gameInterval = setInterval(step, updateTimeout());
+
+  drawCanvas();
+}
--- scripts/interaction.js
+++ scripts/interaction.js
@@ -74,63 +74,6 @@ window.addEventListener("keydown", (event) => {
   }
 });
 
-function stopGame(isSuccessfully) {
-  gameOver = true;
-  gameOverSound.play();
-
-  scoreInGame.style.visibility = "hidden";
-  gameOverScreen.style.visibility = "visible";
-  canvas.style.borderColor = isSuccessfully ? "darkgreen" : "#FFD829";
-
-  saveScore();
-  clearInterval(gameInterval);
-}
-
-function startGame() {
-  gameOver = false;
-  startSound.play();
-
-  score = 0;
-  timeout = 80;
-  directionQueue = [];
-  currentDirection = moveRight;
-  currentSnake = makeInitialSnake(5);
-
-  currentSnakeKeys = new Set();
-  currentVacantKeys = new Set();
-
-  scoreInGame.style.visibility = "visible";
-  gameOverScreen.style.visibility = "hidden";
-
-  for (let top = 0; top < ROWS; top += 1) {
-    for (let left = 0; left < COLS; left += 1) {
-      currentVacantKeys.add(toKey([top, left]));
-    }
-  }
-
-  for (let cell of currentSnake) {
-    const key = toKey(cell);
-
-    currentVacantKeys.delete(key);
-    currentSnakeKeys.add(key);
-  }
-
-  currentFoodKey = spawnFood();
-
-  const [snakeKeys, vacantKeys] = partitionCells(currentSnake);
-
-  currentSnakeKeys = snakeKeys;
-  currentVacantKeys = vacantKeys;
-
-  canvas.style.borderColor = "";
-  gameOverScore.innerHTML = score;
-  scoreInGame.innerHTML = score;
-
-  gameInterval = setInterval(step, updateTimeout());
-
-  drawCanvas();
-}
-
 start.addEventListener("click", (event) => {
   event.preventDefault();
 
